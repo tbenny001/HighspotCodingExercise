200116b7b173ddd7cee64fe06bd0bb6d60d2945e
tbenny <tony.benny@coxautoinc.com>
2021-07-10 23:39:04 -0400

    Creating Tags entity

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'tags', to: "tags#index"
   get 'folders', to: "folders#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_032650) do
+ActiveRecord::Schema.define(version: 2021_07_11_033820) do
 
   create_table "folders", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
