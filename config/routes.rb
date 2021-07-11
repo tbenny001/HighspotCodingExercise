f1d9150c0541ec35ff3dfd821fe4cb2e0b9d7af9
tbenny <tony.benny@coxautoinc.com>
2021-07-11 16:55:12 -0400

    Refactoring routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get 'tags', to: "tags#index"
-  get 'folders', to: "folders#index"
+  scope 'folders', controller: "folder" do
+    get 'folders', to: "folders#index", as: :folders_index
+    post 'folders/create/', to: "folders#create", as: :folder_creation
+  end
+
+  scope 'tags', controller: "tag" do
+    get 'tags', to: "tags#index", as: :tags_index
+    post 'tags/create/', to: "tags#create", as: :tag_creation
+  end
 end
