1dc2f7e14009a83c2dacd05f04cae1178b9f11a6
tbenny <tony.benny@coxautoinc.com>
2021-07-12 09:05:07 -0400

    Refactoring search code

--- app/controllers/url_items_controller.rb
+++ app/controllers/url_items_controller.rb
@@ -3,16 +3,20 @@ class UrlItemsController < ApplicationController
     tags = Tag.where(name: params[:tags])
     if params.include?(:tags)
       raise ActiveRecord::RecordInvalid if tags.empty?
-    else
-    UrlItem.create!(url: params[:url], name: params[:name], folder_id: params[:folder_id].to_i, description: params[:description], tags: tags)
     end
+
+    UrlItem.create!(url: params[:url], name: params[:name], folder_id: params[:folder_id].to_i, description: params[:description], tags: tags)
+    render status: :ok, json: { message: "Url Item successfully created" }
   rescue ActiveRecord::RecordInvalid => e
     render status: :bad_request, json: { error: "Invalid parameter passed. Please try again" }
   end
 
   def search
-    @url_items = UrlItem.where(name: params[:name])
-    @url_items = @url_items.joins(:tags).includes(:tags).where(tags: {name: params[:tags]})
+    if params[:tags]
+      @url_items = UrlItem.where(name: params[:name]).joins(:tags).includes(:tags).where(tags: {name: params[:tags]})
+    else
+      @url_items = UrlItem.where(name: params[:name])
+    end
     @url_items = @url_items.where(folder_id: params[:folder_id]) if params[:folder_id]
     render json: @url_items.to_json
   end
