c97a86a835f0b7b74306b3a66bfda0f570a23421
tbenny <tony.benny@coxautoinc.com>
2021-07-12 00:37:10 -0400

    Cleaning up routes and disabling CSRF token security

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   scope 'folders', controller: "folder" do
-    get 'folders', to: "folders#index", as: :folders_index
-    post 'folders/create/', to: "folders#create", as: :folder_creation
+    get 'index', to: "folders#index", as: :folders_index
+    post 'create', to: "folders#create", as: :folder_creation
   end
 
   scope 'tags', controller: "tag" do
-    get 'tags', to: "tags#index", as: :tags_index
-    post 'tags/create/', to: "tags#create", as: :tag_creation
+    get 'index', to: "tags#index", as: :tags_index
+    post 'create', to: "tags#create", as: :tag_creation
   end
 
   scope 'url_items', controller: "url_items" do
-    post 'url_items/create/', to: "url_items#create", as: :url_item_creation
-    post 'url_items/search/', to: "url_items#search", as: :url_item_search
+    post 'create', to: "url_items#create", as: :url_item_creation
+    post 'search', to: "url_items#search", as: :url_item_search
   end
 end
